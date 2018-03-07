660631660b256cb4a5a7e747202dd90b2073a23f
fernandoloway <fernando.loway@gmail.com>
2018-03-07 17:03:58 +0700

    2nd commit (fix bug form submit)

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,22 +5,24 @@ class LoginForm extends Component {
 
 
     handleSubmit = () => {
-        let user= { name: this.refs.nama.value};
+        let user = { name: this.refs.nama.value };
         this.props.onSubmit(user);
-        
+
     }
 
     render() {
         return (
-            <form className="form-horizontal">
-            <div className="form-group">
-              <label htmlFor="name" className="col-sm-12 control-label" id="inputname">Name</label>
-              <div className="col-sm-12">
-                <input type="text" ref="nama" className="form-control" id="name" placeholder="Lintang" />
-              </div>
+            <div>
+                <form className="form-horizontal">
+                    <div className="form-group">
+                        <label htmlFor="name" className="col-sm-12 control-label" id="inputname">Name</label>
+                        <div className="col-sm-12">
+                            <input type="text" ref="nama" className="form-control" id="name" placeholder="Lintang" />
+                        </div>
+                    </div>
+                </form>
+                <button className="btn btn-info" onClick={this.handleSubmit} >Submit</button>
             </div>
-            <button className="btn btn-info" onClick={this.handleSubmit} >Submit</button>
-          </form>
         );
     }
 }
